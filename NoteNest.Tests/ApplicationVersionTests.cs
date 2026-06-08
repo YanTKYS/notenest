b489b118e3affcfe8cc0d226df389ae0d7ba8f9a
YanTKYS <agri066127@gmail.com>
2026-06-08 11:20:52 +0900

    v1.3.6: 責務分離の第五段階を実装

---
+++ NoteNest.Tests/ApplicationVersionTests.cs
@@ -0,0 +1,30 @@
+using NoteNest.Models;
+using NoteNest.ViewModels;
+using Xunit;
+
+namespace NoteNest.Tests;
+
+public class ApplicationVersionTests
+{
+    [Fact]
+    public void ApplicationVersion_UsesAssemblyInformationalVersion()
+    {
+        Assert.Equal("1.3.6", MainViewModel.ApplicationVersion);
+    }
+
+    [Fact]
+    public void WindowTitle_UsesApplicationVersion()
+    {
+        var viewModel = new MainViewModel();
+
+        Assert.EndsWith(" - ver1.3.6", viewModel.WindowTitle);
+        Assert.DoesNotContain($" - ver{Project.CurrentSchemaVersion}", viewModel.WindowTitle);
+    }
+
+    [Fact]
+    public void ApplicationVersion_IsIndependentFromProjectSchemaVersion()
+    {
+        Assert.Equal("1.3.1", Project.CurrentSchemaVersion);
+        Assert.NotEqual(Project.CurrentSchemaVersion, MainViewModel.ApplicationVersion);
+    }
+}
